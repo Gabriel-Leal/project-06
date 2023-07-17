43e8bc69ed3440dba6275a6090533d354f2a7fce
Gabriel-Leal <gabriel.leal.s.oliveira@gmail.com>
2023-07-17 19:24:47 -0400

    setting Header component and finishing style on the Lesson component

--- src/components/Lesson/index.tsx
+++ src/components/Lesson/index.tsx
@@ -1,13 +1,19 @@
-import { Video } from "lucide-react";
+import { PlayCircle, Video } from "lucide-react";
 // import { useDispatch } from "react-redux";
 // import { play } from "../../store/slices/player";
 
 interface LessonProps {
   title: string;
   duration: string;
+  isCurrent?: boolean;
   onPlay: () => void;
 }
-export function Lesson({ title, duration, onPlay }: LessonProps) {
+export function Lesson({
+  title,
+  duration,
+  onPlay,
+  isCurrent = false,
+}: LessonProps) {
   // const dispatch = useDispatch();
 
   // function handlePlayLesson() {
@@ -17,9 +23,16 @@ export function Lesson({ title, duration, onPlay }: LessonProps) {
   return (
     <button
       onClick={onPlay}
-      className="flex items-center gap-3 text-sm text-zinc-400"
+      data-active={isCurrent}
+      className="flex items-center gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100"
+      disabled={isCurrent}
     >
-      <Video className="w-4 h-4 text-zinc-500" />
+      {isCurrent ? (
+        <PlayCircle className="w-4 h-4 text-emerald-400" />
+      ) : (
+        <Video className="w-4 h-4 text-zinc-500" />
+      )}
+
       <span>{title}</span>
       <span className="ml-auto font-mono text-xs text-zinc-500">
         {duration}
