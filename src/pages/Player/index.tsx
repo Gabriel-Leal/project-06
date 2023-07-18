f7a82569f046ec83f32f6f3ce97c2a1088e09646
Gabriel-Leal <gabriel.leal.s.oliveira@gmail.com>
2023-07-17 20:16:10 -0400

    create a hook for the function current lesson

--- src/pages/Player/index.tsx
+++ src/pages/Player/index.tsx
@@ -4,11 +4,18 @@ import { Header } from "../../components/Header";
 import { VideoPlayer } from "../../components/VideoPlayer";
 import * as Accordion from "@radix-ui/react-accordion";
 import { useAppSelector } from "../../store";
+import { useCurrentLesson } from "../../store/slices/player";
+import { useEffect } from "react";
 
 export function Player() {
   const modules = useAppSelector((state) => {
     return state.player.course.modules;
   });
+
+  const { currentLesson } = useCurrentLesson();
+  useEffect(() => {
+    document.title = `Watching: ${currentLesson.title}`;
+  }, [currentLesson]);
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,4 +1,5 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { useAppSelector } from "..";
 
 // lessons.find(lesson => lesson.id === activeLesson)
 const playerSlice = createSlice({
@@ -57,4 +58,13 @@ const playerSlice = createSlice({
 
 export const player = playerSlice.reducer;
 
-export const {play, next} = playerSlice.actions
+export const {play, next} = playerSlice.actions;
+
+export const useCurrentLesson = () => {
+    return  useAppSelector((state) => {
+        const { currentLessonIndex, currentModuleIndex } = state.player;
+        const currentModule = state.player.course.modules[currentModuleIndex];
+        const currentLesson = currentModule.lessons[currentLessonIndex];
+        return { currentModule, currentLesson };
+      });
+}
